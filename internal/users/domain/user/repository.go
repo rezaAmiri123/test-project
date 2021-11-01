d416ca6f7dcf97755519dfae0ed46abaa9dd7773
reza amiri <reza.amiri.ac@gmail.com>
2021-11-01 10:00:06 +0330

    added user repository

---
+++ internal/users/domain/user/repository.go
@@ -0,0 +1,11 @@
+package user
+
+import "context"
+
+type Repository interface {
+	Create(ctx context.Context, user *User)error
+	Update(ctx context.Context, user *User)error
+	GetByEmail(ctx context.Context, email string)(*User,error)
+	GetByUsername(ctx context.Context, username string)(*User,error)
+}
+
