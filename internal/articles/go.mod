945b660ba10a1b8189af67054c37ed980bd829b9
reza amiri <reza.amiri.ac@gmail.com>
2021-11-13 10:41:17 +0330

    added article web server

--- internal/articles/go.mod
+++ internal/articles/go.mod
@@ -3,6 +3,7 @@ module github.com/rezaAmiri123/test-project/internal/articles
 go 1.16
 
 require (
+	github.com/go-chi/chi v1.5.4
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/gosimple/slug v1.11.2
---
+++ internal/articles/ports/http.go
@@ -0,0 +1,84 @@
+package ports
+
+import (
+	"encoding/json"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/rezaAmiri123/test-project/internal/articles/app"
+	"github.com/rezaAmiri123/test-project/internal/articles/domain/article"
+	"net/http"
+)
+
+type HttpServer struct {
+	app *app.Application
+}
+
+type HttpConfig struct {
+	Addr string
+}
+
+func NewHttpServer(config *HttpConfig, application *app.Application) (*http.Server, error) {
+	srv := &http.Server{
+		Addr: config.Addr,
+	}
+	httpServer := &HttpServer{app: application}
+	router := newRouter(httpServer)
+	srv.Handler = router
+	return srv, nil
+}
+
+func newRouter(httpServer *HttpServer) chi.Router {
+	apiRouter := chi.NewRouter()
+	setMiddlewares(apiRouter)
+	apiRouter.Route("/articles", func(r chi.Router) {
+		r.Post("/create", httpServer.CreateArticle)
+		r.Get("/article/{slug}", httpServer.GetArticle)
+	})
+	rootRouter := chi.NewRouter()
+	rootRouter.Mount("/api/v1", apiRouter)
+	return rootRouter
+}
+
+func (h *HttpServer) CreateArticle(w http.ResponseWriter, r *http.Request) {
+	a := &article.Article{}
+	if err := json.NewDecoder(r.Body).Decode(a); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	userUUID := "xxxxxxxxxxxxxxxxxxxxxxx"
+	if err := h.app.Commands.CreateArticle.Handle(r.Context(), a, userUUID); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (h *HttpServer) GetArticle(w http.ResponseWriter, r *http.Request) {
+	articleSlug := chi.URLParam(r, "slug")
+	a, err := h.app.Queries.GetArticle.Handle(r.Context(), articleSlug)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(a)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func setMiddlewares(router *chi.Mux) {
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
+	//router.Use(logs.NewStructuredLogger(logrus.StandardLogger()))
+	router.Use(middleware.Recoverer)
+
+	//addCorsMiddleware(router)
+	//addAuthMiddleware(router)
+
+	router.Use(
+		middleware.SetHeader("X-Content-Type-Options", "nosniff"),
+		middleware.SetHeader("X-Frame-Options", "deny"),
+	)
+	router.Use(middleware.NoCache)
+}
